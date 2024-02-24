e18b33aa9f9a34eacfd32be500a33c638255b632
Petronio <petroniodasilvafigueiredo@gmail.com>
2024-02-24 02:32:21 -0300

    Update nvim configurations OK project ongoing...

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,9 @@
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set relativenumber")
+vim.cmd("set nu")
+vim.keymap.set('n', '<A-Tab>', ":tabnext <CR>", {})
+vim.g.mapleader = " "
