6672e6096bc110b9869b933967fbf6d15d2b6888
Petronio <petroniodasilvafigueiredo@gmail.com>
2024-02-26 09:09:47 -0300

    Update: linting and Alpha Dasboard

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -6,7 +6,13 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.diagnostics.eslint,
+				null_ls.builtins.formatting.prettier, -- Use Mason to install prettier
+				null_ls.builtins.formatting.misspell, -- Use Mason to install misspell
+				null_ls.builtins.formatting.black, -- Use Mason to install black <python
+				null_ls.builtins.formatting.isort, -- Use Mason to install isort <python
+				null_ls.builtins.diagnostics.rubocop,
+				null_ls.builtins.diagnostics.eslint_d,-- Use Mason to install eslint_d
+				null_ls.builtins.formatting.rubocop,  -- Use Mason to install rubocop
 				null_ls.builtins.completion.spell,
 			},
 		})
--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -4,8 +4,10 @@ return {
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 
+		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" }
+
 		configs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+			auto_isntall = true,
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
